8eec2be3721f6797c37b048c0c3ca1ef1b769494
Mert Karsiyakalilar <mert.karsiyakalilar@gmail.com>
2017-01-08 20:41:40 -0500

    fix: adding more file extensions due to breaking mobile uploads

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ app.config['ASSET_FOLDER'] = './assets/'
 app.config["WATERMARK_IMAGE"] = "./assets/water.png"
 
 # These are the extension that we are accepting to be uploaded
-app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])
+app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'JPG', 'JPEG'])
 
 ## start and schedule jobs
 def maintain_dirs():
