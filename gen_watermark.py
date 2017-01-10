a19fe22b78ea7daf2cb1f7ddc7c84b244e8e9f56
Mert Karsiyakalilar <mert.karsiyakalilar@gmail.com>
2017-01-09 22:30:51 -0500

    fix: adding orientation logic for phone pictures

--- gen_watermark.py
+++ gen_watermark.py
@@ -2,6 +2,53 @@
 from PIL import Image, ImageEnhance
 import random
 
+def flip_horizontal(im): return im.transpose(Image.FLIP_LEFT_RIGHT)
+def flip_vertical(im): return im.transpose(Image.FLIP_TOP_BOTTOM)
+def rotate_180(im): return im.transpose(Image.ROTATE_180)
+def rotate_90(im): return im.transpose(Image.ROTATE_90)
+def rotate_270(im): return im.transpose(Image.ROTATE_270)
+def transpose(im): return rotate_90(flip_horizontal(im))
+def transverse(im): return rotate_90(flip_vertical(im))
+orientation_funcs = [None,
+                 lambda x: x,
+                 flip_horizontal,
+                 rotate_180,
+                 flip_vertical,
+                 transpose,
+                 rotate_270,
+                 transverse,
+                 rotate_90
+                ]
+def apply_orientation(im):
+    """
+    Extract the oritentation EXIF tag from the image, which should be a PIL Image instance,
+    and if there is an orientation tag that would rotate the image, apply that rotation to
+    the Image instance given to do an in-place rotation.
+
+    :param Image im: Image instance to inspect
+    :return: A possibly transposed image instance
+    """
+
+    try:
+        kOrientationEXIFTag = 0x0112
+        if hasattr(im, '_getexif'): # only present in JPEGs
+            e = im._getexif()       # returns None if no EXIF data
+            if e is not None:
+                print("exif found")
+                orientation = e[kOrientationEXIFTag]
+                f = orientation_funcs[orientation]
+                return f(im)
+            else:
+                print("exif exists as a func but no value returned")
+                return im
+        else:
+            print("no exif found")
+            return im
+
+    except:
+        print("problem applying orientation to the image")
+        return(im)
+
 def reduce_opacity(im, opacity):
     """Returns an image with reduced opacity."""
     assert opacity >= 0 and opacity <= 1
@@ -15,12 +62,24 @@ def reduce_opacity(im, opacity):
     return im
 
 def watermark(im, mark, position, opacity=1):
-    """Adds a watermark to an image."""
+    """
+        Adds a watermark to an image.
+        change opacity
+        apply orientation
+        resize image
+        apply watermark
+    """
+    # change opacity
     if opacity < 1:
         mark = reduce_opacity(mark, opacity)
-    if im.mode != 'RGBA':
-        im = im.convert('RGBA')
 
+    # if im.mode != 'RGBA':
+    #     im = im.convert('RGBA')
+
+    # apply orientation
+    im = apply_orientation(im)
+
+    # resize image
     if max(im.size) > 1000:
         _aspect_ratio = float(im.size[0]) / float(im.size[1])
         if _aspect_ratio >= 1:
@@ -32,8 +91,7 @@ def watermark(im, mark, position, opacity=1):
 
         im = im.resize((new_width, new_height), Image.ANTIALIAS)
 
-    # create a transparent layer the size of the image and draw the
-    # watermark in that layer.
+    # apply watermark
     layer = Image.new('RGBA', im.size, (0,0,0,0))
     if position == 'tile':
         for y in range(0, im.size[1], mark.size[1]):
@@ -49,10 +107,10 @@ def watermark(im, mark, position, opacity=1):
         layer.paste(mark, (0,0))
     else:
         layer.paste(mark, position)
+    
     # composite the watermark with the layer
     print("generated image")
     return Image.composite(layer, im, layer)
-
 
 def generate(img_path, out_path, watermark_path):
   print(img_path)
@@ -66,10 +124,9 @@ def generate(img_path, out_path, watermark_path):
             position="scale",
             opacity=0.5).save(out_path, 'JPEG')
 
-
 def test():
-    img_path = "./uploads/a_syrian_in_istanbul_by_canankk-d7zppui.jpg"
-    out_path = "./target_images/wm_a_syrian_in_istanbul_by_canankk-d7zppui.jpg"
+    img_path = "./uploads/IMG_0014.JPG"
+    out_path = "./target_images/wm_IMG_0014.JPG"
     watermark_path = "./assets/water.png"
     generate(img_path, out_path, watermark_path)
 
